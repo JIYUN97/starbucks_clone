40b5136c5497bc99184fa4d1e100b1a1249361f4
silano_08 <henzel1013@gmail.com>
2021-04-05 16:58:22 +0900

    [변수충돌]

--- schemas/Category.js
+++ schemas/Category.js
@@ -9,11 +9,7 @@ const CategorySchema = new Schema({
     type: String,
     required: true,
   },
-<<<<<<< HEAD
-  imgage: {
-=======
   image: {
->>>>>>> 49cdd37923bfd56f2b8138de190ccb0e528e099d
     type: String,
     required: true,
   },
