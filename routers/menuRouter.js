c3d9ec793c05cf1498d9871d7e58202a0e0cc66e
YOOJIYUN <ys7638@naver.com>
2021-04-05 15:12:28 +0900

    카테고리 api,음료별 상세 api

--- routers/menuRouter.js
+++ routers/menuRouter.js
@@ -3,9 +3,36 @@ const menuRouter = express.Router();
 const { Category } = require("../schemas/Category");
 const { Menu } = require("../schemas/Menu");
 
-menuRouter.get("/", async (req, res) => {
+//전체메뉴에서 음료 api
+menuRouter.get("/drink", async (req, res) => {
   try {
     let result = await Category.find({});
+    return res.send({ result });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err: err.message });
+  }
+});
+
+//카테고리별 페이지 api
+menuRouter.get("/drink/categories/:categoryId", async (req, res) => {
+  try {
+    let { categoryId } = req.params;
+    let category = await Category.findOne({ _id: categoryId });
+    let category_name = category.eng_name;
+    let result = await Menu.find({ eng_category: category_name });
+    return res.send({ result });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ err: err.message });
+  }
+});
+
+//음료별 페이지 api
+menuRouter.get("/drink/:menuId", async (req, res) => {
+  try {
+    let { menuId } = req.params;
+    let result = await Menu.findOne({ _id: menuId });
     return res.send({ result });
   } catch (err) {
     console.log(err);
