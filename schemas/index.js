4494a80657027f892d832f7e1be70d6cdef19dbe
YOOJIYUN <ys7638@naver.com>
2021-04-10 17:37:13 +0900

    create schemas index.js

---
+++ schemas/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ...require("./Cart"),
+  ...require("./Category"),
+  ...require("./Menu"),
+  ...require("./Mymenu"),
+  ...require("./User"),
+  ...require("./UserHistory"),
+};
